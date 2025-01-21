84b3af3efe299aae199720a1fec42f57473185d4
D33key <dimakisa777@yandex.ru>
2025-01-21 12:50:38 +0300

    touch reactivity and see difference between native and delegative events

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal, Show } from 'solid-js';
+import './App.css';
+import Child from './components/Child';
 
 function App() {
-  const [count, setCount] = createSignal(0)
+	const [count, setCount] = createSignal(0);
+	const [inputValue, setInputValue] = createSignal('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
+	const handler = (data: string, event: MouseEvent) => {
+		console.log('Data:', data, 'Event:', event);
+	};
+
+	return (
+		<div style='display: flex; flex-direction: column; gap: 20px;'>
+			<Show
+				when={count() >= 5}
+				fallback={
+					<button on:click={() => {
+            console.log('Clicked')
+            setCount((count) => count + 1)
+            }}>
+						count is {count()}
+					</button>
+				}
+			>
+				<Child />
+			</Show>
+			<hr style='width: 100%' />
+			<div>
+				<p
+					classList={{
+						red: inputValue() === 'red',
+						yellow: inputValue() === 'yellow',
+					}}
+				>
+					Type red or yellow to change color of this text!
+				</p>
+				<p>Input text value: {inputValue()}</p>
+				<input
+					type='text'
+					value={inputValue()}
+					onInput={(event) => setInputValue(event.target.value)}
+				/>
+			</div>
+			<hr style='width: 100%' />
+			<div>
+				<button onClick={[handler, 'Hello!']}>Click Me</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Child.tsx
@@ -0,0 +1,5 @@
+function Child() {
+	return <div>Limit is reached!</div>;
+}
+
+export default Child;
